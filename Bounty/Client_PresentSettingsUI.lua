6e822eca6ab0e567b322ddf6ad363811ae30c542
Martin Bouchard <marbouchl@gmail.com>
2026-04-07 23:05:57 -0400

    add setting ui for bounty

--- Bounty/Client_PresentSettingsUI.lua
+++ Bounty/Client_PresentSettingsUI.lua
@@ -1,5 +1,7 @@
 ---Client_PresentSettingsUI hook
 ---@param rootParent RootParent
 function Client_PresentSettingsUI(rootParent)
-
+	UI.CreateLabel(rootParent).SetText("Bounty Mode")
+	UI.CreateLabel(rootParent)
+		.SetText("Bounty reward: " .. tostring(Mod.Settings.BountyReward) .. " armies per elimination")
 end
